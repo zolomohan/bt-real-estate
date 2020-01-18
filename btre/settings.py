b957d38f019a45168af3c235c12f7672070de3fa
Mohan Raj <mohanzolomon@gmail.com>
2020-01-18 18:33:23 +0530

    Add Listings & Realtors apps and Static Listings Pages

--- btre/settings.py
+++ btre/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Custom Apps
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig'
 ]
 
 MIDDLEWARE = [
