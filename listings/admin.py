26be5835d238805a535f9a4dc9481d050ff45f98
zolomohan <mohanzolomon@gmail.com>
2020-01-19 15:53:04 +0530

    Customize Admin View

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'realtor', 'list_date', 'is_published')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', 'state', 'city')
+    list_editable = ('is_published',)
+    search_fields = ('title', 'state', 'city', 'price', 'zipcode', 'address', 'description')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
