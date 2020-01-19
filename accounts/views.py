97f410a53bd79eb103e93345da246d630d836dcd
zolomohan <mohanzolomon@gmail.com>
2020-01-20 00:17:27 +0530

    Add Messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def login(request):
     if request.method == 'POST':
-        pass
+        messages.error(request, 'Testing Error Message')
+        return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 def register(request):
     if request.method == 'POST':
-        pass
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -1,2 +1,4 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+setTimeout(() => $('#message').fadeOut('slow'), 3000);
