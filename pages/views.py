57b04e35d03315e05ef6ba10bd9b7bb88f8bdde1
zolomohan <mohanzolomon@gmail.com>
2020-01-19 17:19:28 +0530

    Add Recent Listings on Home Page

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from listings.models import Listing
 
 def index(request):
-    return render(request, 'pages/index.html')
+    recent_listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        "recent_listings": recent_listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
