6733b66b7806794ae9dd3d9a0178d7d9938cdae5
zolomohan <mohanzolomon@gmail.com>
2020-01-19 17:58:54 +0530

    Add Realtors, Seller of the Month in About Page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     recent_listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -10,4 +11,11 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')[:3]
+    mvp = Realtor.objects.all().filter(is_mvp=True)
+    if mvp: mvp = mvp[0]
+    context = {
+        "realtors": realtors,
+        "mvp": mvp
+    }
+    return render(request, 'pages/about.html', context)
