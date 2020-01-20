1e16b2f396e1d4610f5366d3f3e760f80469decb
zolomohan <mohanzolomon@gmail.com>
2020-01-20 23:11:30 +0530

    Add Dynamic Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
+from contacts.models import Contact
+
 
 def register(request):
     if request.method == 'POST':
@@ -49,7 +51,11 @@ def login(request):
 
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contacted_at').filter(user_id=request.user.id)
+    context = {
+        "contacts": user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def logout(request):
