cfb9c5d8ab1595e6a0ee1af7df186e4a0540adf4
zolomohan <mohanzolomon@gmail.com>
2020-01-20 22:34:00 +0530

    Add POST Contact Funtionality

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .models import Contact
 
 # Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        user_id = request.POST['user_id']
+        listing = request.POST['listing']
+        listing_id = request.POST['listing_id']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing=listing, listing_id=listing_id, user_id=user_id, name=name, email=email, phone=phone, message=message)
+        contact.save()
+
+        messages.success(request, "Your Inquiry has been submitted, The realtor will get back to you soon.")
+        return redirect('/listings/'+listing_id)
